f336878e66512430eb9ecf99397bb79bb82339d0
Edwin Quintuna <Edwin.Quintuna82@myhunter.cuny.edu>
2018-11-26 09:11:53 -0500

    11/26/2018 Commit

--- onlineStore/src/app/home-page/home-page.component.ts
+++ onlineStore/src/app/home-page/home-page.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemService } from '../services/dbAccess/item.service';
+import { CategoryService } from '../services/dbAccess/category.service';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent{
+  Items$;
+  Categories$;
 
-  constructor() { }
+  constructor(private itemService: ItemService, private categoryService: CategoryService) { 
+    this.Items$ = itemService.getAll()
+    .map(prods => prods
+      .map(p => p.payload.val()));
+      this.Categories$ = this.categoryService.getCategories();
+  }
 
   ngOnInit() {
   }
